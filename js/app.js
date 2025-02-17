8fc51e491006da3bcb178d429ab3d0bbe74ea419
OCTO <chobotnicecz@gmail.com>
2025-02-17 20:34:39 +0100

    getDetails refactor

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { template } from "./data.js";
+
 const root = document.getElementById("root");
 
 function createButton(text) {
@@ -52,44 +54,15 @@ function showItems(data, type) {
   });
 }
 
-function createDetailElement(label, value) {
-  const detailElement = document.createElement("h5");
-  detailElement.textContent = `${label}: ${value}`;
-  return detailElement;
-}
-
 function getDetails(data, type) {
-  if (type === "people") {
-    return [
-      { label: "Gender", value: data.gender },
-      { label: "Height", value: data.height },
-      { label: "Mass", value: data.mass },
-      { label: "Birth Year", value: data.birth_year },
-      { label: "Skin Color", value: data.skin_color },
-      { label: "Hair Color", value: data.hair_color },
-    ];
-  } else if (type === "planets") {
-    return [
-      { label: "Population", value: data.population },
-      { label: "Diameter", value: data.diameter },
-      { label: "Rotation Period", value: data.rotation_period },
-      { label: "Climate", value: data.climate },
-      { label: "Terrain", value: data.terrain },
-      { label: "Gravity", value: data.gravity },
-      { label: "Surface Water", value: data.surface_water },
-    ];
-  } else if (type === "starships") {
-    return [
-      { label: "Model", value: data.model },
-      { label: "Manufacturer", value: data.manufacturer },
-      { label: "Passengers", value: data.passengers },
-      { label: "Credits", value: data.cost_in_credits },
-      { label: "Length", value: data.length },
-      { label: "Starship Class", value: data.starship_class },
-      { label: "Max Speed", value: data.max_atmosphering_speed },
-    ];
-  }
-  return [];
+  const details = [];
+  const templateData = template[type];
+
+  templateData.forEach(({ label, key }) => {
+    details.push({ label, value: data[key] });
+  });
+
+  return details;
 }
 
 function createDetailElement(label, value) {
@@ -173,6 +146,13 @@ function showLoader() {
   root.appendChild(loader);
 }
 
+function moveBottom() {
+  return {
+    top: document.body.scrollHeight,
+    behavior: "smooth",
+  };
+}
+
 function createResponseApi(type) {
   let itemUrl = `https://swapi.dev/api/${type}`;
 
@@ -198,10 +178,7 @@ function createResponseApi(type) {
     }
 
     showItems(data.results, type);
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: "smooth",
-    });
+    window.scrollTo(moveBottom());
 
     loader.classList.add("disable");
     loader.classList.remove("move");
